86ea90ba8c1ae75ca68cac916fb0a27beda58056
Aratimius <aratimius@gmail.com>
2023-09-14 16:21:24 +0300

    Упс! Перепутал проекты...
    This reverts commit ff5cd23b122de2b654bc1c74952735016831ef45.

--- src/channel.py
+++ src/channel.py
@@ -1,7 +1,3 @@
-import json
-import os
-
-from googleapiclient.discovery import build
 
 
 class Channel:
@@ -9,11 +5,8 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        pass
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        api_key: str = os.getenv('YT_API_KEY')
-        youtube = build('youtube', 'v3', developerKey=api_key)
-        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-        print(json.dumps(channel, indent=2, ensure_ascii=False))
+        pass
